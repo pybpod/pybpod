6328816d4f88bc394d2ca9a67a2b1e07edd089b8
Ricardo Ribeiro <ricardojvr@gmail.com>
2017-11-20 13:48:11 +0000

    fix install script for windows

--- install.py
+++ install.py
@@ -1,17 +1,17 @@
-import pip
+import pip, os
 
 
 def install():
-	pip.main(['install', '--upgrade', 'logging-bootstrap/.'])
-	pip.main(['install', '--upgrade', 'pysettings/.'])
-	pip.main(['install', '--upgrade', 'pyforms/.'])
-	pip.main(['install', '--upgrade', 'pyforms-generic-editor/.'])
-	pip.main(['install', '--upgrade', 'pybpod-api/.'])
-	pip.main(['install', '--upgrade', 'pybpod-gui-api/.'])
-	pip.main(['install', '--upgrade', 'pybpod-gui-plugin/.'])
-	pip.main(['install', '--upgrade', 'pybpod-gui-plugin-session-history/.'])
-	pip.main(['install', '--upgrade', 'pybpod-gui-plugin-timeline/.'])
-	pip.main(['install', '--upgrade', 'pybpod-rotary-encoder-module/.'])
+	pip.main(['install', '--upgrade', os.path.join('logging-bootstrap','.')])
+	pip.main(['install', '--upgrade', os.path.join('pysettings','.')])
+	pip.main(['install', '--upgrade', os.path.join('pyforms','.')])
+	pip.main(['install', '--upgrade', os.path.join('pyforms-generic-editor','.')])
+	pip.main(['install', '--upgrade', os.path.join('pybpod-api','.')])
+	pip.main(['install', '--upgrade', os.path.join('pybpod-gui-api','.')])
+	pip.main(['install', '--upgrade', os.path.join('pybpod-gui-plugin','.')])
+	pip.main(['install', '--upgrade', os.path.join('pybpod-gui-plugin-session-history','.')])
+	pip.main(['install', '--upgrade', os.path.join('pybpod-gui-plugin-timeline','.')])
+	pip.main(['install', '--upgrade', os.path.join('pybpod-rotary-encoder-module','.')])
 
 
 if __name__=='__main__': 
