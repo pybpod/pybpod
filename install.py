3d8f39b94c6a017c046facc1b0d3cde23df2be2c
Ricardo Ribeiro <ricardojvr@gmail.com>
2017-11-20 15:01:45 +0000

    Updated submodule pybpod-gui-plugin

--- install.py
+++ install.py
@@ -17,6 +17,16 @@ SUBMODULES_FOLDERS = [
     'pybpod-rotary-encoder-module'
 ]
 
+DEFAULT_PLUGINS = [
+    'pybpodgui_plugin',
+    'pybpodgui_plugin_timeline',
+    'pybpodgui_plugin_session_history',
+    'pybpod-rotary-encoder-module'
+]
+
+
+
+
 def install():
     for submodule in SUBMODULES_FOLDERS:
         pip.main(['install', '--upgrade', os.path.join(submodule,'.')])
@@ -27,6 +37,14 @@ def check_submodules():
             call(["git", "pull", "--recurse-submodules"])
             break
 
+def conf_default_settings():
+    if not os.path.exists('user_settings.py'):
+        f = open('user_settings.py', 'w')
+        f.write("SETTINGS_PRIORITY = 0\n\n")
+        f.write("GENERIC_EDITOR_PLUGINS_LIST = "+str(DEFAULT_PLUGINS))
+        f.close()
+
 
 if __name__=='__main__': 
     install()
+    conf_default_settings()
