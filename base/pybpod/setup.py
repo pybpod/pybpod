2b96b717548085c8a9f2797fbd07b836395521b6
Luís Teixeira <micboucinha@gmail.com>
2019-05-03 15:22:13 +0100

    Update version to v1.7.1

--- base/pybpod/setup.py
+++ base/pybpod/setup.py
@@ -8,7 +8,7 @@ with open(os.path.join('README.md'), 'r') as fd:
 
 setup(
     name='pybpod',
-    version="1.7.0",
+    version="1.7.1",
     description="Pybpod is a behavioral experiments control system written in Python for Bpod",
     author=['Ricardo Ribeiro', 'Luís Teixeira'],
     author_email='ricardo.ribeiro@research.fchampalimaud.org, ricardojvr@gmail.com, micboucinha@gmail.com',
