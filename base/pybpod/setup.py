99512ae9ce3c55536ffe917f0cbbc44c5bd665ac
Luís Teixeira <micboucinha@gmail.com>
2019-12-09 17:30:12 +0000

    Bump version to v1.8.1

--- base/pybpod/setup.py
+++ base/pybpod/setup.py
@@ -8,7 +8,7 @@ with open(os.path.join("README.md"), "r") as fd:
 
 setup(
     name="pybpod",
-    version="1.8.0",
+    version="1.8.1",
     description="Pybpod is a behavioral experiments control system written in Python for Bpod",
     author=["Ricardo Ribeiro", "Luís Teixeira"],
     author_email="ricardo.ribeiro@research.fchampalimaud.org, ricardojvr@gmail.com, micboucinha@gmail.com",
@@ -21,9 +21,9 @@ setup(
     install_requires=[
         "pyforms-gui==4.901.2",
         "pyforms_generic_editor==1.5.1",
-        "pybpod-api==1.8.0",
-        "pybpod-gui-api==1.8.0",
-        "pybpod-gui-plugin==1.8.0",
+        "pybpod-api==1.8.1",
+        "pybpod-gui-api==1.8.1",
+        "pybpod-gui-plugin==1.8.1",
         "pge-plugin-terminal==0.1",
         "pybpod-gui-plugin-session-history==1.4.2",
         "pybpod-gui-plugin-stmdiagram==1.0.0",
