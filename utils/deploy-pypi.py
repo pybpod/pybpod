09f44caa581a81763f6ac2bf9948cfb6c01dea51
Luís Teixeira <micboucinha@gmail.com>
2019-12-12 17:18:50 +0000

    Run black on utils scripts

--- utils/deploy-pypi.py
+++ utils/deploy-pypi.py
@@ -9,17 +9,17 @@ def version_compare(loc_version, rmt_version):
     return v.parse(loc_version) > v.parse(rmt_version)
 
 
-pypi = xmlrpc.client.ServerProxy('https://pypi.org')
+pypi = xmlrpc.client.ServerProxy("https://pypi.org")
 
 DIRECTORIES_TO_SEARCH_FORM = [
-    os.path.join('libraries'),
-    os.path.join('base'),
-    os.path.join('plugins'),
+    os.path.join("libraries"),
+    os.path.join("base"),
+    os.path.join("plugins"),
 ]
 
 CURRENT_DIRECTORY = os.getcwd()
 
-Popen(['pip', 'install', '--upgrade', 'setuptools', 'wheel', 'twine'])
+Popen(["pip", "install", "--upgrade", "setuptools", "wheel", "twine"])
 
 for search_dir in DIRECTORIES_TO_SEARCH_FORM:
     for dir_name in os.listdir(search_dir):
@@ -27,30 +27,39 @@ for search_dir in DIRECTORIES_TO_SEARCH_FORM:
         if not os.path.isdir(dir_path):
             continue
 
-        setup_filepath = os.path.join(dir_path, 'setup.py')
+        setup_filepath = os.path.join(dir_path, "setup.py")
         if not os.path.isfile(setup_filepath):
             continue
 
         os.chdir(dir_path)
 
-        version = Popen(["python", setup_filepath, '--version'], stdout=PIPE).stdout.read()
+        version = Popen(
+            ["python", setup_filepath, "--version"], stdout=PIPE
+        ).stdout.read()
         version = version.strip().decode()
 
-        package_name = Popen(["python", setup_filepath, '--name'], stdout=PIPE).stdout.read()
-        package_name = package_name.strip().decode().replace(' ', '-')
+        package_name = Popen(
+            ["python", setup_filepath, "--name"], stdout=PIPE
+        ).stdout.read()
+        package_name = package_name.strip().decode().replace(" ", "-")
 
         remote_version = pypi.package_releases(package_name)
 
         print(dir_name, version, remote_version)
 
-        if len(remote_version) == 0 or version_compare(version, remote_version[0]) is True:
-            print('----- UPLOADING TO PYPI -----', package_name)
+        if (
+            len(remote_version) == 0
+            or version_compare(version, remote_version[0]) is True
+        ):
+            print("----- UPLOADING TO PYPI -----", package_name)
 
-            if os.path.isdir('./dist'):
-                shutil.rmtree('./dist')
+            if os.path.isdir("./dist"):
+                shutil.rmtree("./dist")
 
-            Popen(['python', 'setup.py', 'sdist', 'bdist_wheel'], stdout=PIPE).communicate()
-            Popen(['twine', 'upload', os.path.join('dist', '*')]).communicate()
+            Popen(
+                ["python", "setup.py", "sdist", "bdist_wheel"], stdout=PIPE
+            ).communicate()
+            Popen(["twine", "upload", os.path.join("dist", "*")]).communicate()
 
         os.chdir(CURRENT_DIRECTORY)
 
--- utils/install.py
+++ utils/install.py
@@ -6,49 +6,47 @@ from subprocess import call
 
 SUBMODULES_FOLDERS = [
     # LIBRARIES
-    'libraries/logging-bootstrap',
-    'libraries/pyforms-gui',
-    'libraries/pyforms-generic-editor',
-    'libraries/safe-collaborative-architecture',
-
+    "libraries/logging-bootstrap",
+    "libraries/pyforms-gui",
+    "libraries/pyforms-generic-editor",
+    "libraries/safe-collaborative-architecture",
     # BASE
-    'base/pybpod-api',
-    'base/pybpod-gui-api',
-    'base/pybpod-gui-plugin',
-
+    "base/pybpod-api",
+    "base/pybpod-gui-api",
+    "base/pybpod-gui-plugin",
     # PLUGINS
-    'plugins/pge-plugin-terminal',
-    'plugins/pybpod-gui-plugin-alyx',
-    'plugins/pybpod-gui-plugin-emulator',
-    'plugins/pybpod-gui-plugin-session-history',
-    'plugins/pybpod-gui-plugin-stmdiagram',
-    'plugins/pybpod-gui-plugin-timeline',
-    'plugins/pybpod-gui-plugin-trial-timeline',
-    'plugins/pybpod-gui-plugin-waveplayer',
-    'plugins/pybpod-gui-plugin-rotaryencoder',
-    'plugins/pybpod-gui-plugin-soundcard',
+    "plugins/pge-plugin-terminal",
+    "plugins/pybpod-gui-plugin-alyx",
+    "plugins/pybpod-gui-plugin-emulator",
+    "plugins/pybpod-gui-plugin-session-history",
+    "plugins/pybpod-gui-plugin-stmdiagram",
+    "plugins/pybpod-gui-plugin-timeline",
+    "plugins/pybpod-gui-plugin-trial-timeline",
+    "plugins/pybpod-gui-plugin-waveplayer",
+    "plugins/pybpod-gui-plugin-rotaryencoder",
+    "plugins/pybpod-gui-plugin-soundcard",
 ]
 
 DEFAULT_PLUGINS = [
-    'pybpodgui_plugin',
-    'pybpodgui_plugin_timeline',
-    'pybpodgui_plugin_session_history',
+    "pybpodgui_plugin",
+    "pybpodgui_plugin_timeline",
+    "pybpodgui_plugin_session_history",
 ]
 
 
 def install():
     for submodule in SUBMODULES_FOLDERS:
-        call(['pip', 'install', '-e', os.path.join(submodule, '.')])
+        call(["pip", "install", "-e", os.path.join(submodule, ".")])
 
 
 def conf_default_settings():
-    if not os.path.exists('user_settings.py'):
-        f = open('user_settings.py', 'w')
+    if not os.path.exists("user_settings.py"):
+        f = open("user_settings.py", "w")
         f.write("SETTINGS_PRIORITY = 0\n\n")
-        f.write("GENERIC_EDITOR_PLUGINS_LIST = "+str(DEFAULT_PLUGINS))
+        f.write("GENERIC_EDITOR_PLUGINS_LIST = " + str(DEFAULT_PLUGINS))
         f.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     install()
     conf_default_settings()
