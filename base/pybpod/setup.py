0655f1ec7939a7d2978b181c6df350afc71bfa53
Luís Teixeira <micboucinha@gmail.com>
2019-12-12 17:02:24 +0000

    Normalize version numbering on setup.py

--- base/pybpod/setup.py
+++ base/pybpod/setup.py
@@ -3,12 +3,14 @@
 import os
 from setuptools import setup
 
+current_version = "1.8.1"
+
 with open(os.path.join("README.md"), "r") as fd:
     long_description = fd.read()
 
 setup(
     name="pybpod",
-    version="1.8.1",
+    version=f"{current_version}",
     description="Pybpod is a behavioral experiments control system written in Python for Bpod",
     author=["Ricardo Ribeiro", "Luís Teixeira"],
     author_email="ricardo.ribeiro@research.fchampalimaud.org, ricardojvr@gmail.com, micboucinha@gmail.com",
@@ -21,9 +23,9 @@ setup(
     install_requires=[
         "pyforms-gui==4.901.2",
         "pyforms_generic_editor==1.5.1",
-        "pybpod-api==1.8.1",
-        "pybpod-gui-api==1.8.1",
-        "pybpod-gui-plugin==1.8.1",
+        f"pybpod-api=={current_version}",
+        f"pybpod-gui-api=={current_version}",
+        f"pybpod-gui-plugin=={current_version}",
         "pge-plugin-terminal==0.1",
         "pybpod-gui-plugin-session-history==1.4.2",
         "pybpod-gui-plugin-stmdiagram==1.0.0",
@@ -34,7 +36,5 @@ setup(
         "pybpod-gui-plugin-soundcard==0.1.6",
         "pybpod-gui-plugin-emulator==0.1.4",
     ],
-    entry_points={
-        "console_scripts": ["start-pybpod=pybpodgui_plugin.__main__:start"],
-    },
+    entry_points={"console_scripts": ["start-pybpod=pybpodgui_plugin.__main__:start"]},
 )
